123d71dae83b80cd96d74378056359aedf428093
Luís Henrique Pereira <lh.p@hotmail.com>
2021-10-14 21:24:08 -0300

    Buildin a new project rentX

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.View`
+
+`;
+
+export const Title = styled.Text`
+
+`;
